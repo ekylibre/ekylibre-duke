c434c069463f3012023526fe88117f7230a930ac
manuel <mlabous@ekylibre.com>
2020-10-08 14:44:07 +0200

    Config.yml added inside gem, removed "ibm_skd" & "lodash" gems

--- lib/duke/engine.rb
+++ lib/duke/engine.rb
@@ -4,5 +4,10 @@ module Duke
       app.config.assets.precompile += %w( duke.js )
       puts "app.config.assets.precompile #{app.config.assets.precompile }"
     end
+
+    initializer :i18n do |app|
+      app.config.i18n.load_path += Dir[Duke::Engine.root.join('config', 'locales', '**', '*.yml')]
+    end
+
   end
 end
