639f0ccd28aa2f90eae69c2ff1dc47d51b53c652
MSJarre <mlabous@ekylibre.com>
2020-11-17 12:38:29 +0100

    Logging HTTP requests in sidekiq

--- app/workers/request_worker.rb
+++ app/workers/request_worker.rb
@@ -3,8 +3,9 @@ class RequestWorker
   include HTTParty
   include Pusher
 
+
   def perform(url, body, headers, session_id)
-    response = HTTParty.post(HTTP::URI.parse(url), :headers => HTTP::Headers.coerce(headers), :body => body.to_json)
+    response = HTTParty.post(HTTP::URI.parse(url), :headers => HTTP::Headers.coerce(headers), :body => body.to_json, :debug_output => $stdout)
     Pusher.trigger(session_id, 'my-event', {
       message: response['output']['generic']
     })
