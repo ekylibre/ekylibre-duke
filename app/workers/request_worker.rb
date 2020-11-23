9dcc77afeb1e240d8774d12ecd6239fedeefb2a8
MSJarre <mlabous@ekylibre.com>
2020-11-23 13:03:45 +0100

    Js cleaning, better UX

--- app/workers/request_worker.rb
+++ app/workers/request_worker.rb
@@ -6,7 +6,7 @@ class RequestWorker
 
   def perform(url, body, headers, session_id)
     response = HTTParty.post(HTTP::URI.parse(url), :headers => HTTP::Headers.coerce(headers), :body => body.to_json, :debug_output => $stdout)
-    Pusher.trigger(session_id, 'my-event', {
+    Pusher.trigger(session_id, 'duke', {
       message: response['output']['generic']
     })
   end
