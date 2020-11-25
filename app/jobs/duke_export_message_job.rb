d6037ec78cd993efc6939709a7699e6be1476e92
MSJarre <mlabous@ekylibre.com>
2020-11-25 16:34:59 +0100

    Waiting for Pusher WS to be initialized before sending first message

--- app/jobs/duke_export_message_job.rb
+++ app/jobs/duke_export_message_job.rb
@@ -4,12 +4,8 @@ class DukeExportMessageJob < ApplicationJob
 
   def perform(item_id, session_id)
     I18n.locale = :fra
-    # HOTFIX: doing it only once because watson fires two webhooks at the time of writing
-    # TODO : Stop Watson from sending two webhooks for a single event
-    if item_id.even?
-      Pusher.trigger(session_id, 'duke', {
-        message: [{"response_type":"text","text": I18n.t("duke.exports.export_over" , id: item_id)}]
-      })
-    end 
+    Pusher.trigger(session_id, 'duke', {
+      message: [{"response_type":"text","text": I18n.t("duke.exports.export_over" , id: item_id)}]
+    })
   end
 end
