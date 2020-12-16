e81a465d94d0f5885cee924af8472304ca590169
MSJarre <mlabous@ekylibre.com>
2020-12-16 12:34:49 +0100

    Reversing ActionCable to Pusher to fast-prod

--- app/jobs/duke_export_message_job.rb
+++ app/jobs/duke_export_message_job.rb
@@ -1,9 +1,10 @@
 class DukeExportMessageJob < ApplicationJob
+  include Pusher
   queue_as :duke
 
   def perform(item_id, session_id)
-    ActionCable.server.broadcast 'duke',
-      message: [{"response_type":"text","text": I18n.t("duke.exports.export_over" , id: item_id)}],
-      room_id: session_id
+    Pusher.trigger(session_id, 'duke', {
+      message: [{"response_type":"text","text": I18n.t("duke.exports.export_over" , id: item_id)}]
+    })
   end
 end
--- app/jobs/duke_request_job.rb
+++ app/jobs/duke_request_job.rb
@@ -1,11 +1,12 @@
 class DukeRequestJob < ApplicationJob
   include HTTParty
+  include Pusher
   queue_as :duke
 
   def perform(url, body, headers, session_id)
     response = HTTParty.post(HTTP::URI.parse(url), :headers => HTTP::Headers.coerce(headers), :body => body.to_json)
-    ActionCable.server.broadcast 'duke',
-      message: response['output']['generic'],
-      room_id: session_id
+    Pusher.trigger(session_id, 'duke', {
+      message: response['output']['generic']
+    })
   end
 end
