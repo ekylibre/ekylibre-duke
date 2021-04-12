033bbe60dd7d042e5cedf0278c5470e9e7e9f3dd
MSJarre <mlabous@ekylibre.com>
2021-04-12 17:08:15 +0200

    Authentication with user.email & user.authentication_token

--- app/controllers/duke/duke_webhooks_controller.rb
+++ app/controllers/duke/duke_webhooks_controller.rb
@@ -1,6 +1,8 @@
 module Duke
   class DukeWebhooksController < ApplicationController
+    # Maybe ovveride verify_authentificity_token with authenticate_by_token method ??
     skip_before_action :verify_authenticity_token
+    before_action :authenticate_by_token
     
     def handle_webhook
       event = ActiveSupport::HashWithIndifferentAccess.new(params[:main_param])
@@ -11,6 +13,17 @@ module Duke
           render json: response
         end 
       end 
+    end
+
+    private 
+
+    # check if token concords with user email for authentication
+    def authenticate_by_token
+      if User.find_by_email(request.env['HTTP_EKYLIBRE_EMAIL']).authentication_token == request.env['HTTP_EKYLIBRE_TOKEN']
+        handle_webhook
+      else 
+        logger.info('Unable to authenticate the user')
+      end
     end
 
   end
