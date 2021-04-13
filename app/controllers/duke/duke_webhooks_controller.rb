90d6ba84a243b77a09bf00911f8e2b3519201bf9
MSJarre <mlabous@ekylibre.com>
2021-04-13 15:22:23 +0200

    Routing to authentication verification

--- app/controllers/duke/duke_webhooks_controller.rb
+++ app/controllers/duke/duke_webhooks_controller.rb
@@ -1,29 +1,26 @@
 module Duke
   class DukeWebhooksController < ApplicationController
     # Maybe ovveride verify_authentificity_token with authenticate_by_token method ??
-    skip_before_action :verify_authenticity_token
-    before_action :authenticate_by_token
-    
-    def handle_webhook
-      event = ActiveSupport::HashWithIndifferentAccess.new(params[:main_param])
-      class_ = ("Duke::Skill::#{event[:hook_skill]}").constantize.new
-      Ekylibre::Tenant.switch event[:tenant] do
-        I18n.with_locale(:fra) do
-          response = class_.send "handle_#{event[:hook_request]}", event
-          render json: response
-        end 
-      end 
+    skip_before_action :verify_authenticity_token    
+
+    # check if token concords with user email for authentication
+    def authenticate_by_token
+      Ekylibre::Tenant.switch params[:main_param][:tenant] do
+        if User.find_by(authentication_token: request.env['HTTP_EKYLIBRE_TOKEN']).email == request.env['HTTP_EKYLIBRE_EMAIL']
+          handle_webhook
+        end
+      end
     end
 
     private 
 
-    # check if token concords with user email for authentication
-    def authenticate_by_token
-      if User.find_by_email(request.env['HTTP_EKYLIBRE_EMAIL']).authentication_token == request.env['HTTP_EKYLIBRE_TOKEN']
-        handle_webhook
-      else 
-        logger.info('Unable to authenticate the user')
-      end
+    def handle_webhook
+      event = ActiveSupport::HashWithIndifferentAccess.new(params[:main_param])
+      class_ = ("Duke::Skill::#{event[:hook_skill]}").constantize.new
+      I18n.with_locale(:fra) do
+        response = class_.send "handle_#{event[:hook_request]}", event
+        render json: response
+      end 
     end
 
   end
