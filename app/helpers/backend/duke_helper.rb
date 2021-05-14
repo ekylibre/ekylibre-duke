c3ffc8a708d0c79d193c291e9b0d67b9010aa8fd
MSJarre <mlabous@ekylibre.com>
2021-05-14 20:58:10 +0200

    Adding helpers & JSS/SCSS requires

---
+++ app/helpers/backend/duke_helper.rb
@@ -0,0 +1,9 @@
+module Backend
+  module DukeHelper
+
+    def duke_tag
+      render(partial: 'layouts/duke')
+    end
+
+  end
+end
--- lib/duke/engine.rb
+++ lib/duke/engine.rb
@@ -8,5 +8,24 @@ module Duke
       app.config.i18n.load_path += Dir[Duke::Engine.root.join('config', 'locales', '**', '*.yml')]
     end
 
+    initializer :hack_plugin_stylesheet do
+      Rails.root.join('tmp', 'plugins', 'theme-addons', 'themes', 'tekyla', 'plugins.scss').open('a') do |f|
+        f.write <<~SCSS
+          // Hack for gem-only plugins to inject css (Duke)
+          @import "duke.scss";
+        SCSS
+      end
+    end
+
+    initializer :hack_plugin_javascript do
+      Rails.root.join('tmp', 'plugins', 'javascript-addons', 'plugins.js.coffee').open('a') do |f|
+        f.write('#= require duke')
+      end
+    end
+
+    initializer :duke_helpers do
+      ActionView::Base.send :include, Backend::DukeHelper
+    end
+
   end
 end
