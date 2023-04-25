ceec3f89906fdc073fe492333dd0789b522ccaf2
Thibaut <tgorioux@ekylibre.com>
2023-04-25 15:02:54 +0200

    Don't raise if env variable is missing

--- config/initializers/watson.rb
+++ config/initializers/watson.rb
@@ -1,8 +1,4 @@
-raise_missing_env_var = ->(var) do
-  raise "Missing Duke ENV variable: #{var}"
-end
-
-WATSON_APIKEY = ENV.fetch('WATSON_APIKEY', &raise_missing_env_var)
-WATSON_URL = ENV.fetch('WATSON_URL', &raise_missing_env_var)
-WATSON_VERSION = ENV.fetch('WATSON_VERSION', &raise_missing_env_var)
-WATSON_ID = ENV.fetch('WATSON_ID', &raise_missing_env_var)
+WATSON_APIKEY = ENV.fetch('WATSON_APIKEY')
+WATSON_URL = ENV.fetch('WATSON_URL')
+WATSON_VERSION = ENV.fetch('WATSON_VERSION')
+WATSON_ID = ENV.fetch('WATSON_ID')
